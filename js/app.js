835e72b862fb2decbeb280e53ea59d3f734d67fa
Brian <Brian@Brians-MacBook-Air.local>
2019-04-09 10:38:23 -0500

    varibales declared now working on functions

--- js/app.js
+++ js/app.js
@@ -3,14 +3,43 @@
 class MemoryGame{
 constructor(){
 this.storage = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-this.iconRecord = [];
+this.imgRec = [];
 this.rand;
-this.flipCount = 0;
-}
+this.cardtextRec = [];
+this.cardRec = [];
+this.cardNum
+this.front
+this.back
+this.memory = $('.memory-game')
 
 }
 
+}
+
+const game = {
+  flipCount: 0,
+  cardCheck: 0,
+  correct:0,
+  status:0,
+  gameOver:false,
 
 
 
+flipCardId(){
+  memory.on('click', function(e){
+    let el = e.target.parent()
+    let numId = el.id;
+    if(Number.isInteger(parseInt(numId.replace("back", ""),10))){
+      cardClick(el.parentElement.id);
+    }
+    else{
+      cardClick(el.id);
+    }
 
+    
+     
+  });
+}
+
+
+}
